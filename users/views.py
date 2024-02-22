86f2510b4ae83e9c253aba5677703e69a399c02e
sinan <sinanalungal012@gmail.com>
2024-02-22 15:13:17 +0530

    after formatting the code

--- users/views.py
+++ users/views.py
@@ -5,6 +5,7 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 from django.contrib.auth import get_user_model
 from openai import OpenAI
+from django.db.models import Q
 from image_generator.settings import OPEN_AI_API_KEY
 from .serializers import (
     UserCreateSerializer,
@@ -15,7 +16,6 @@ from .serializers import (
     UpdateUserEdit,
     UpdateUserPassword
 )
-from django.db.models import Q
 
 User = get_user_model()
 
@@ -84,8 +84,14 @@ class ImageView(APIView):
         
 
 class ProfilePicView(APIView):
+    """
+    API endpoint for updating the profile picture.
+    """
+
     def patch(self, request):
-        print(request.data)
+        """
+        Handle PATCH request for updating the profile picture.
+        """
         try:
             image_data = request.data.get('profile')
         except KeyError:
@@ -107,24 +113,19 @@ class ProfilePicView(APIView):
         except User.DoesNotExist:
             return Response({"error": 'User not found'}, status=status.HTTP_400_BAD_REQUEST)
 
-    # def put(self, request,id):
-    #     try:
-    #         user_obj = User.objects.get(id=id)
-    #     except User.DoesNotExist:
-    #         return Response("User profile not found", status=status.HTTP_404_NOT_FOUND)
-
-    #     serializer = ProfilePicSerializer(user_obj, data=request.data)
-    #     if serializer.is_valid():
-    #         serializer.save()
-    #         return Response(serializer.data)
-    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
 
 class RetrieveUserData(APIView):
-    def post(self,request):
+    """
+    API endpoint for retrieving user data.
+    """
+
+    def post(self, request):
+        """
+        Handle POST request for retrieving user data.
+        """
         email = request.data.get('email')
         try:
-            data=User.objects.get(email = email)
+            data = User.objects.get(email=email)
         except User.DoesNotExist:
             return Response({"error": 'There is no user'}, status=status.HTTP_400_BAD_REQUEST)
         user = UserDataSerializer(data)
@@ -132,6 +133,10 @@ class RetrieveUserData(APIView):
     
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    """
+    Custom token serializer to include additional user information in the token.
+    """
+
     @classmethod
     def get_token(cls, user):
         token = super().get_token(user)
@@ -142,7 +147,6 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
         token['isSuperuser'] = user.is_superuser
         token['profile'] = user.profile.url if user.profile else None
 
-
         return token
 
 
@@ -150,88 +154,120 @@ class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
     
 class UserDataUpdateView(APIView):
-    def post(self,request):
+    """
+    API endpoint for updating user data.
+    """
+
+    def post(self, request):
+        """
+        Handle POST request for updating user data.
+        """
         try:
-            user_obj=User.objects.get(email=request.data.get('email'))
+            user_obj = User.objects.get(email=request.data.get('email'))
         except User.DoesNotExist:
-            return Response({"error": "User does not exist"},status=status.HTTP_400_BAD_REQUEST)
+            return Response({"error": "User does not exist"}, status=status.HTTP_400_BAD_REQUEST)
         
         if request.data.get('password'):
-            serializer = UpdateUserEdit(user_obj,data=request.data)
+            serializer = UpdateUserEdit(user_obj, data=request.data)
         else:
-            serializer = UpdateUserEdit(user_obj,data=request.data)
+            serializer = UpdateUserEdit(user_obj, data=request.data)
 
         if serializer.is_valid():
             if request.data.get('password'):
-                serializerpassword=UpdateUserPassword(user_obj,data=request.data)
-                if serializerpassword.is_valid()  and (request.data['password']).strip()!='':
+                serializerpassword = UpdateUserPassword(user_obj, data=request.data)
+                if serializerpassword.is_valid() and (request.data['password']).strip() != '':
                     user_obj.set_password(request.data.get('password'))
             serializer.save()
             return Response(serializer.data)
-        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 class AdminDataFetch(APIView):
-    def post(self,request):
+    """
+    API endpoint for fetching data for admin users.
+    """
+
+    def post(self, request):
+        """
+        Handle POST request for fetching data for admin users.
+        """
         try:
-            admin=User.objects.get(email=request.data.get('email'))
+            admin = User.objects.get(email=request.data.get('email'))
             if admin.is_superuser:
-                allData=User.objects.all().exclude(is_superuser=True).order_by('id')
-                serializer=UserDataSerializer(allData,many=True)
-                return Response(serializer.data,status=status.HTTP_200_OK)
+                all_data = User.objects.all().exclude(is_superuser=True).order_by('id')
+                serializer = UserDataSerializer(all_data, many=True)
+                return Response(serializer.data, status=status.HTTP_200_OK)
             else:
-                return Response({"error": "You are not an admin"},status=status.HTTP_400_BAD_REQUEST)
+                return Response({"error": "You are not an admin"}, status=status.HTTP_400_BAD_REQUEST)
         except User.DoesNotExist:
-            return Response({"error": "User does not exist"},status=status.HTTP_400_BAD_REQUEST)
+            return Response({"error": "User does not exist"}, status=status.HTTP_400_BAD_REQUEST)
         
 class EditUserData(APIView):
-    def put(self,request,id):
+    """
+    API endpoint for editing user data.
+    """
 
+    def put(self, request, id):
+        """
+        Handle PUT request for editing user data.
+        """
         print(request.data)
-
         print(id)
         try:
-            user_obj=User.objects.get(id=id)
+            user_obj = User.objects.get(id=id)
         except User.DoesNotExist:
-            return Response({"error":'User does not exist'},status=status.HTTP_400_BAD_REQUEST)
+            return Response({"error": 'User does not exist'}, status=status.HTTP_400_BAD_REQUEST)
         try:
-            if "username" in request.data and (request.data["username"]).strip()!='':
-                user_obj.username=request.data["username"]
-            if "email" in request.data and (request.data["email"]).strip()!='':
-                user_obj.email=request.data["email"]
-            if "phone_number" in request.data and (request.data["phone_number"]).strip()!='':
-                user_obj.phone_number=request.data["phone_number"]
+            if "username" in request.data and (request.data["username"]).strip() != '':
+                user_obj.username = request.data["username"]
+            if "email" in request.data and (request.data["email"]).strip() != '':
+                user_obj.email = request.data["email"]
+            if "phone_number" in request.data and (request.data["phone_number"]).strip() != '':
+                user_obj.phone_number = request.data["phone_number"]
             if 'is_listed' in request.data:
-                user_obj.is_listed=request.data["is_listed"]
-            if "password" in request.data and (request.data["password"]).strip()!='':
+                user_obj.is_listed = request.data["is_listed"]
+            if "password" in request.data and (request.data["password"]).strip() != '':
                 user_obj.set_password(request.data["password"])
             user_obj.save()
-            datas=User.objects.all().exclude(is_superuser=True).order_by('id')
-            serializer=UserDataSerializer(datas,many=True)
-            return Response(serializer.data,status=status.HTTP_200_OK)
+            datas = User.objects.all().exclude(is_superuser=True).order_by('id')
+            serializer = UserDataSerializer(datas, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         except ValueError:
-            return Response({"error":'Something wentwrong'},status=status.HTTP_400_BAD_REQUEST)
+            return Response({"error": 'Something went wrong'}, status=status.HTTP_400_BAD_REQUEST)
 
 class DeleteUser(APIView):
-    def delete(self,request,id):
+    """
+    API endpoint for deleting a user.
+    """
+
+    def delete(self, request, id):
+        """
+        Handle DELETE request for deleting a user.
+        """
         try:
-            user_obj=User.objects.get(id=id)
+            user_obj = User.objects.get(id=id)
             user_obj.delete()
-            usersData=User.objects.all().exclude(is_superuser=True).order_by('id')
-            serialize=UserDataSerializer(usersData,many=True)
-            return Response(serialize.data,status=status.HTTP_200_OK)
+            users_data = User.objects.all().exclude(is_superuser=True).order_by('id')
+            serialize = UserDataSerializer(users_data, many=True)
+            return Response(serialize.data, status=status.HTTP_200_OK)
         except User.DoesNotExist:
-            return Response({"error":"User Does not exist"},status=status.HTTP_400_BAD_REQUEST)
+            return Response({"error": "User Does not exist"}, status=status.HTTP_400_BAD_REQUEST)
         
 class SearchQuery(APIView):
-    def post(self,request):
-        search=(request.data.get('search')).strip()
-        # print(search)
+    """
+    API endpoint for searching users based on a query.
+    """
+
+    def post(self, request):
+        """
+        Handle POST request for searching users based on a query.
+        """
+        search = (request.data.get('search')).strip()
         try:
-            if search!='':
-                usersData=User.objects.filter(Q(username__icontains=search) | Q(email__icontains=search) | Q(phone_number__icontains=search)).exclude(is_superuser=True).all().order_by('id')
+            if search != '':
+                users_data = User.objects.filter(Q(username__icontains=search) | Q(email__icontains=search) | Q(phone_number__icontains=search)).exclude(is_superuser=True).all().order_by('id')
             else:
-                usersData=User.objects.all().exclude(is_superuser=True).order_by('id')
-            serialize=UserDataSerializer(usersData,many=True)
-            return Response(serialize.data,status=status.HTTP_200_OK)
-        except:
-            return Response({"error":"Something went wrong"},status=status.HTTP_400_BAD_REQUEST)
+                users_data = User.objects.all().exclude(is_superuser=True).order_by('id')
+            serialize = UserDataSerializer(users_data, many=True)
+            return Response(serialize.data, status=status.HTTP_200_OK)
+        except User.DoesNotExist:
+            return Response({"error": "Something went wrong"}, status=status.HTTP_400_BAD_REQUEST)
